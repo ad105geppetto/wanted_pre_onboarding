f1cc61ee23cd9c13497b3410e1cd790b3291bde9
ad105geppetto <ad105geppetto@gmail.com>
2022-08-19 19:14:46 +0900

    [feat] Add search api

--- index.js
+++ index.js
@@ -1,30 +1,78 @@
 const http = require("http");
-const { companys, recruitments } = require("./models");
+const { Op } = require("sequelize");
+const { companys, recruitments, sequelize } = require("./models");
 
 const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   const method = req.method;
+  const url = new URL(req.url, `http://${req.host}`);
   try {
     if (method === "GET") {
-      recruitments
-        .findAll({
-          include: [
-            {
-              model: companys,
-              attributes: ["companyname", "country", "region"],
+      if (url.pathname === "/api") {
+        recruitments
+          .findAll({
+            attributes: {
+              include: [
+                [sequelize.col("companyname"), "companyname"],
+                [sequelize.col("country"), "country"],
+                [sequelize.col("region"), "region"],
+              ],
+              exclude: ["companyId", "createdAt", "updatedAt"],
             },
-          ],
-        })
-        .then((data) => {
-          res.statusCode = 200;
-          res.setHeader("Content-Type", "application/json");
-          return res.end(JSON.stringify({ data: data, message: "채용공고 목록을 가져옵니다." }));
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+            include: [
+              {
+                model: companys,
+                attributes: [],
+              },
+            ],
+          })
+          .then((data) => {
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "application/json");
+            return res.end(JSON.stringify({ data: data, message: "채용공고 목록을 가져옵니다." }));
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+      if (url.pathname === "/api/some/url") {
+        const keyword = url.searchParams.get("search");
+        console.log(keyword);
+        recruitments
+          .findAll({
+            where: {
+              [Op.or]: [
+                { position: { [Op.like]: `%${keyword}%` } },
+                { techStack: { [Op.like]: `%${keyword}%` } },
+                { $companyname$: { [Op.like]: `%${keyword}%` } },
+              ],
+            },
+            attributes: {
+              include: [
+                [sequelize.col("companyname"), "companyname"],
+                [sequelize.col("country"), "country"],
+                [sequelize.col("region"), "region"],
+              ],
+              exclude: ["companyId", "createdAt", "updatedAt"],
+            },
+            include: [
+              {
+                model: companys,
+                attributes: [],
+              },
+            ],
+          })
+          .then((data) => {
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "application/json");
+            return res.end(JSON.stringify({ data: data, message: "채용공고 목록을 가져옵니다." }));
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
     } else if (method === "POST") {
       let body = [];
 
--- models/recruitments.js
+++ models/recruitments.js
@@ -10,6 +10,10 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       this.belongsTo(models.companys);
+      this.belongsToMany(models.users, {
+        through: "users_recuitments",
+        foreignKey: "recruitmentId",
+      });
     }
   }
   recruitments.init(
